a8c75e334ed38647d8b713c11fd34e7fc3cee41b
Tuffi Saliba Neto <tuffisal@gmail.com>
2021-01-25 22:28:41 -0200

    Inserindo Identity Core na API

--- ProAgil.API/Helpers/AutoMapperProfiles.cs
+++ ProAgil.API/Helpers/AutoMapperProfiles.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using AutoMapper;
 using ProAgil.API.Dtos;
 using ProAgil.Domain;
+using ProAgil.Domain.Identity;
 
 namespace ProAgil.API.Helpers
 {
@@ -21,6 +22,8 @@ namespace ProAgil.API.Helpers
 
             CreateMap<Lote,LoteDto>().ReverseMap();
             CreateMap<RedeSocial,RedeSocialDto>().ReverseMap();
+            CreateMap<User,UserDto>().ReverseMap();
+            CreateMap<User,UserLoginDto>().ReverseMap();
         }
     }
 }
